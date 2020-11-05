5a8851784906bb71bca52a465ec02dd32897d9e2
yuhao <2958044267@qq.com>
2020-11-05 14:15:18 +0800

    更新1

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Hit!");
+        System.out.println("更新1");
     }
 }
